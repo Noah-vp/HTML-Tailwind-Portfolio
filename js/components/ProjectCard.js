485baca488f1ddcccfe811de6b27f775bf7e0f35
Noah-VP <noah.v.potten@gmail.com>
2026-01-18 20:53:57 +0100

    Update ProjectCard.js

--- js/components/ProjectCard.js
+++ js/components/ProjectCard.js
@@ -45,18 +45,16 @@ function createProjectCard(project) {
   mainContainer.appendChild(container);
 
   // Preview image on the right
-  if (project.preview) {
-    const imageContainer = document.createElement("div");
-    imageContainer.className = "flex-shrink-0 self-start flex items-start";
-    
-    const image = document.createElement("img");
-    image.src = project.preview;
-    image.alt = `${project.title} preview`;
-    image.className = "w-32 h-32 object-cover rounded-lg border border-white/10 aspect-square";
-    
-    imageContainer.appendChild(image);
-    mainContainer.appendChild(imageContainer);
-  }
+  const imageContainer = document.createElement("div");
+  imageContainer.className = "flex-shrink-0 self-start flex items-start";
+  
+  const image = document.createElement("img");
+  image.src = project.preview;
+  image.alt = `${project.title} preview`;
+  image.className = "w-32 h-32 object-cover rounded-lg border border-white/10 aspect-square";
+  
+  imageContainer.appendChild(image);
+  mainContainer.appendChild(imageContainer);
 
   article.appendChild(mainContainer);
 
